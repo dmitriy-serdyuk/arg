f88053dc0cc7b9eaeba61418759d272784d81066
Dmitriy Serdyuk <serdyuk.dmitriy@gmail.com>
2019-04-05 15:10:54 -0400

    Add readme and setup

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+
+setup(
+    name="arg",
+    version="0.0.1",
+    author="Dmitriy Serdyuk",
+    author_email="dmitriy@serdyuk.ml",
+    description="Argparser generator for declarative style configurations",
+    long_description="Argparser generator for declarative style "
+                     "configurations",
+    long_description_content_type="text/markdown",
+    url="https://github.com/dmitriy-serdyuk/arg",
+    packages=['arg'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],)
